2ae809a6fa611d7dc23f5e138a857e075b2ed38f
Jeevan Chavan <jeevanchavan7273@gmail.com>
2026-01-24 17:22:03 +0530

    learning basic api using postman, done mini task to create a note and display

---
+++ day3/server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json());
+
+const notes = [];
+
+
+app.post("/notes",(req,res)=>{
+    notes.push(req.body);
+    res.send("note created");
+})
+
+app.get("/notes",(req,res)=>{
+    res.send(notes);
+})
+
+
+app.listen(3000,()=>{
+    console.log("server listening at port 3000")
+})
