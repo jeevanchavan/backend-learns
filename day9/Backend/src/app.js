c0009b6f5b03d3d70ee5f079c29c7ad3478cd609
Jeevan Chavan <jeevanchavan7273@gmail.com>
2026-02-08 21:44:25 +0530

    today is the day 10, integrated update, delete and create api with react and ui, and most important using build command convert the overall react code into dist folder and moved to backend. and handling wildcard api's using middlewares and used static to display the frontend on the backend like merging them and we can access both from the backend uri and its easy to deploy and manage. Most imp to remember the process of merging

--- day9/Backend/src/app.js
+++ day9/Backend/src/app.js
@@ -1,12 +1,23 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from "url";
+
 import connectDB from './config/db.js'
 import noteModel from './models/NoteModel.js'
+
+// It creates __dirname manually in ES modules.
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express()
 dotenv.config()
 
+app.use(express.static("./public"));
 app.use(express.json())
+app.use(cors())
+
 connectDB()
 
 
@@ -27,7 +38,7 @@ app.get("/api/notes",async (req,res)=>{
     res.status(200).json(getAllNotes)
 })
 
-app.patch("/api/notes/:id",async (req,res)=>{
+app.put("/api/notes/:id",async (req,res)=>{
     const {title,description} = req.body;
     const noteUpdated = await noteModel.findByIdAndUpdate(req.params.id,{title,description},{new:true});
     res.status(200).json(noteUpdated);
@@ -37,6 +48,12 @@ app.delete("/api/notes/:id",async (req,res)=>{
     const {id} = req.params;
     const noteDeleted = await noteModel.findByIdAndDelete(id);
     res.status(200).json(noteDeleted)
+})
+console.log(__dirname);
+
+// wildcard api
+app.use("*name",(req,res)=>{
+    res.sendFile(path.join(__dirname,"..","/public/index.html"));
 })
 
 export default app;
--- day9/Frontend/src/App.jsx
+++ day9/Frontend/src/App.jsx
@@ -3,27 +3,108 @@ import axios from 'axios'
 
 const App = () => {
   const [notes, setNotes] = useState([])
+  const [editNote, setEditNote] = useState(null)
 
-  useEffect(() => {
-    axios.get("http://localhost:3000/api/notes")
+  function fetchNotes(){
+    axios.get("http://localhost:3000/api/notes")  // fetch backend 
     .then((res)=>{
       setNotes(res.data)
     })
     .catch((err)=>{
       console.log(err);
     })
+  }
+
+  useEffect(() => {
+    fetchNotes()
   }, [])
-  
+
+  function handlerSubmit(e){
+    e.preventDefault()
+    const {title,description} = e.target.elements;
+    
+    console.log(title.value,description.value);
+
+    axios.post("http://localhost:3000/api/notes",{
+      title : title.value,
+      description : description.value
+    })
+    .then((res)=>{
+      console.log(res.data);
+
+      fetchNotes()
+    })
+    
+  }
+
+  function handleDeleteNote(noteId){
+    axios.delete("http://localhost:3000/api/notes/"+noteId)
+    .then(()=>{
+      fetchNotes()
+    })
+  }
+
+  function handleNoteUpdate(e){
+    e.preventDefault()
+    axios.put("http://localhost:3000/api/notes/"+editNote._id,{
+      title:editNote.title,
+      description:editNote.description
+    })
+    .then(()=>{
+      setEditNote(null);
+      fetchNotes()
+    })
+  }
 
   return (
-    <div className="notes">
-      {notes.map((note)=>{
-        return <div className="note" key={note._id}>
-          <h1>{note.title}</h1>
-          <p>{note.description}</p>
-        </div>
-      })}
-    </div>
+    <>
+
+      <form className='create-form' onSubmit={handlerSubmit}>
+        <input name='title' type="text" placeholder='Enter title' />
+        <input name='description' type="text" placeholder='Enter description' />
+        <button className='create-btn'>Create Note</button>
+      </form>
+
+      {editNote && 
+        <form className='update-form' onSubmit={(e)=>{
+          handleNoteUpdate(e)
+        }}>
+          <input
+            type="text"
+            value={editNote.title}
+            onChange={(e) =>
+              setEditNote({ ...editNote, title: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            value={editNote.description}
+            onChange={(e) =>
+              setEditNote({ ...editNote, description: e.target.value })
+            }
+          />
+           <button className='update-btn' type="submit">Update</button>
+           <button className='delete-btn' type="button" onClick={()=>{
+            setEditNote(null)
+           }}>Cancel</button>
+        </form>
+      }
+
+      <div className="notes">
+        {notes.map((note)=>{
+          return <div className="note" key={note._id}>
+            <h1>{note.title}</h1>
+            <p>{note.description}</p>
+            <button className='delete-btn' onClick={()=>{
+              handleDeleteNote(note._id);
+            }}>delete</button>
+            <button className='update-btn' onClick={()=>{
+              setEditNote(note);
+            }}>update</button>
+          </div>
+        })}
+      </div>
+    </>
   )
 }
 
