ca1df06d497d1f096268bc993d879059f89b7911
Jeevan Chavan <jeevanchavan7273@gmail.com>
2026-02-15 22:24:14 +0530

    this was day 4. two apis were created to fetech post and post details

--- project1/src/controllers/post.controller.js
+++ project1/src/controllers/post.controller.js
@@ -9,6 +9,7 @@ const imagekit = new ImageKit({
     urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
 })
 
+
 export const createPostController = async (req,res) =>{
     // console.log(req.body,req.file)
 
@@ -45,6 +46,82 @@ export const createPostController = async (req,res) =>{
 
     res.status(201).json({
         message:"post created successfully",
+        post
+    })
+}
+
+export const getPostController = async (req,res)=>{
+    const token = req.cookies.token;
+
+    if(!token){
+        return res.status(401).json({
+            message:"Unauthorized Access"
+        })
+    }
+
+    let decoded =null;
+
+    try {
+        decoded = jwt.verify(token,process.env.JWT_SECRET)
+    } catch (error) {
+        return res.status(401).json({
+            message:"token invalid"
+        })
+    }
+    console.log(decoded)
+
+    const userId = decoded.id;
+
+    const posts = await postModel.find({
+        user : userId,
+    })
+
+    res.status(200).json({
+        message:"Posts fetched successfully",
+        posts
+    })
+}
+
+// post details api
+export const getPostDetailsController = async(req,res)=>{
+    const token = req.cookies.token;
+
+    if(!token){
+        return res.status(401).json({
+            message:"Unauthorized Access"
+        })
+    }
+
+    let decoded = null;
+    try {
+        decoded = jwt.verify(token,process.env.JWT_SECRET);
+    } catch (error) {
+        res.status(401).json({
+            message:"Token invalid"
+        })
+    }
+
+    let userId = decoded.id;
+    let postId = req.params.postId
+
+    const post = await postModel.findById(postId)
+
+    if(!post){
+        return res.status(404).json({
+            message:"Post not found"
+        })
+    }
+
+    const isValidUser = post.user.toString() === userId
+
+    if(!isValidUser){
+        return res.status(403).json({
+            message:"Forbidden Content"
+        })
+    }
+
+    return res.status(200).json({
+        message: "Post fetched Successfully",
         post
     })
 }
--- project1/src/routes/post.routes.js
+++ project1/src/routes/post.routes.js
@@ -1,14 +1,20 @@
 import express from "express"
 import multer from 'multer'
 
-import { createPostController } from "../controllers/post.controller.js";
+import { createPostController, getPostController, getPostDetailsController } from "../controllers/post.controller.js";
 
 const upload = multer({storage:multer.memoryStorage()})
 
 const postRouter = express.Router();
 
-// POST : /api/posts
+// POST : /api/posts/
 
 postRouter.post("/",upload.single("image"),createPostController)
+
+// GET : /api/posts/
+postRouter.get("/",getPostController)
+
+//  GET : /api/posts/details?postId
+postRouter.get("/details/:postId",getPostDetailsController)
 
 export default postRouter;
