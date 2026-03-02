ba28306ad026429cd305541a54e5792886be353d
Jeevan Chavan <jeevanchavan7273@gmail.com>
2026-03-03 00:40:51 +0530

    changes made to deploy on render

--- project1/Backend/src/app.js
+++ project1/Backend/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cookieParser from 'cookie-parser';
-// import cors from 'cors'
+import cors from 'cors'
+
 import path from 'path'
 import { fileURLToPath } from "url";
 
@@ -16,10 +17,10 @@ import userRouter from './routes/user.routes.js';
 const app = express();
 
 app.use(express.static("./public"));
-// app.use(cors({
-//     origin: "http://localhost:5173",
-//     credentials: true
-// }))
+app.use(cors({
+    origin: "https://social-media-app-kx46.onrender.com",
+    credentials: true
+}))
 app.use(express.json())
 app.use(cookieParser())
 
