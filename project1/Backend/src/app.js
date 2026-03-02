2b729dd2c398686774bda02a0b4f13526401da4a
Jeevan Chavan <jeevanchavan7273@gmail.com>
2026-03-03 00:12:49 +0530

    changes made to deploy on render

--- project1/Backend/src/app.js
+++ project1/Backend/src/app.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cookieParser from 'cookie-parser';
-import cors from 'cors'
+// import cors from 'cors'
 import path from 'path'
 import { fileURLToPath } from "url";
 
@@ -16,10 +16,10 @@ import userRouter from './routes/user.routes.js';
 const app = express();
 
 app.use(express.static("./public"));
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+// app.use(cors({
+//     origin: "http://localhost:5173",
+//     credentials: true
+// }))
 app.use(express.json())
 app.use(cookieParser())
 
