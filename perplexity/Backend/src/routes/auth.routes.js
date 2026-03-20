faf87226fc8d230e233b4c5bbd5f9802d4f556c5
Jeevan Chavan <jeevanchavan7273@gmail.com>
2026-03-20 18:32:00 +0530

    day 2, authenticaton part is done implementing api such as login,register,verify-email and get-me

--- perplexity/Backend/src/routes/auth.routes.js
+++ perplexity/Backend/src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 import express from 'express'
-import { registerUser } from '../controllers/auth.controller.js'
-import { registerValidator } from '../validators/auth.validator.js'
+import { getMe, loginUser, registerUser, verifyEmail } from '../controllers/auth.controller.js'
+import { loginValidator, registerValidator } from '../validators/auth.validator.js'
+import { authUser } from '../middlewares/auth.middleware.js'
 
 const authRouter = express.Router()
 
@@ -11,5 +12,31 @@ const authRouter = express.Router()
  * @body { username, email, password }
  */
 authRouter.post('/register',registerValidator,registerUser)
+
+
+/**
+ * @route POST /api/auth/login
+ * @desc Login user and return JWT token
+ * @access Public
+ * @body { email, password }
+ */
+authRouter.post("/login",loginValidator,loginUser)
+
+
+/**
+ * @route GET /api/auth/get-me
+ * @desc Get current logged in user's details
+ * @access Private
+ */
+authRouter.get("/get-me",authUser,getMe)
+
+
+/**
+ * @route GET /api/auth/verify-email
+ * @desc Verify user's email address
+ * @access Public
+ * @query { token }
+ */
+authRouter.get("/verify-email",verifyEmail)
 
 export default authRouter
